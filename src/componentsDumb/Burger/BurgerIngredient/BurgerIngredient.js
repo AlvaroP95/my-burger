e24d767fa2293d5cf93903d88cdfa67083e87956
marioylink11 <alvaropuertas1995@gmail.com>
2019-11-06 11:16:58 -0300

    s08e11 anvorguesa andando n.n

--- src/componentsDumb/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/componentsDumb/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-import classes from './BurgerIngredient';
+import classes from './BurgerIngredient.css';
 
 
 class BurgerIngredient extends Component{
@@ -9,27 +9,27 @@ class BurgerIngredient extends Component{
 
         switch (this.props.type){
             case('bread-bottom'):
-                ingredient = <div className={classes.breadBottom}></div>;
+                ingredient = <div className={classes.BreadBottom}></div>;
                 break;
             case('bread-top'):
             ingredient = (
-                <div className={classes.breadBottom}>
-                        <div className={classes.Seeds1}></div>
-                        <div className={classes.Seeds2}></div>
-                    </div>
+                <div className={classes.BreadTop}>
+                    <div className={classes.Seeds1}></div>
+                    <div className={classes.Seeds2}></div>
+                </div>
                 );;
                 break;
             case('meat'):
-                ingredient = <div className={classes.breadBottom}></div>;
+                ingredient = <div className={classes.Meat}></div>;
                 break;
             case('cheese'):
-                ingredient = <div className={classes.breadBottom}></div>;
+                ingredient = <div className={classes.Cheese}></div>;
                 break;
             case('bacon'):
-                ingredient = <div className={classes.breadBottom}></div>;
+                ingredient = <div className={classes.Bacon}></div>;
                 break;
             case('salad'):
-                ingredient = <div className={classes.breadBottom}></div>;
+                ingredient = <div className={classes.Salad}></div>;
                 break;
             default:
                 ingredient = null;        
