236f425bda91ef35915df11a17a4b06c0d4ef213
marioylink11 <alvaropuertas1995@gmail.com>
2019-11-06 13:15:40 -0300

    s08e13

--- src/componentsDumb/Burger/Burger.js
+++ src/componentsDumb/Burger/Burger.js
@@ -3,11 +3,25 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+    let transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+                return <BurgerIngredient key={igKey + i} type={igKey}/>                
+            });
+        }) //Hasta acá te devuelve un array de arrays[cantIngrediente]
+        .reduce((arr, el) => {//Esto te devuelve un array[cantidadIngredientes] de ingredientes 
+            return arr.concat(el)
+        }, []);
+
+        if(transformedIngredients.length === 0){
+            transformedIngredients = <p>Please start adding ingredients!</p>
+        }
+        console.log(transformedIngredients)
+
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"/>
-            <BurgerIngredient type="cheese"/>
-            <BurgerIngredient type="meat"/>
+            {transformedIngredients}
             <BurgerIngredient type="bread-bottom"/>
         </div>
     );
--- src/containersSmart/BurgerBuilder/BurgerBuilder.js
+++ src/containersSmart/BurgerBuilder/BurgerBuilder.js
@@ -3,10 +3,24 @@ import Helper from '../../hoc/Helper';
 import Burger from '../../componentsDumb/Burger/Burger';
 
 export default class BurgerBuilder extends Component {
+    // constructor(props){ NO MODERNO
+    //     super(props);
+    //     this.state = {
+    //         ...
+    //     }
+    // }
+    state = {
+        ingredients: {
+            salad: 0,
+            bacon: 0,
+            cheese: 0,
+            meat: 0
+        }
+    }
     render() {
         return (
             <Helper>
-                <Burger/>
+                <Burger ingredients={this.state.ingredients}/>
                 <div>Build Control</div>
             </Helper>
         )
